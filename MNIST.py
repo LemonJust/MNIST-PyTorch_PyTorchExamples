52208e376d48a24fb236f4e719b67225a0f88fec
iVishalr <vishalramesh01@gmail.com>
2021-05-19 10:10:11 +0530

    Bug Fixes : DataLoader with num_worker>0 now works

--- MNIST.py
+++ MNIST.py
@@ -28,10 +28,11 @@ test_set = torchvision.datasets.MNIST(
     transform=ToTensor()
 )
 
-cnn_train_configs = TrainerConfig(ckpt_path="./CNNModel.pt",max_epochs=40,learning_rate=4.67e-4,weight_decay=6.423e-4)
-CNN_model = MnistCNN()
-trainer = Trainer(model=CNN_model,train_dataset=train_set,test_dataset=test_set,config=cnn_train_configs)
-model_metrics = trainer.train()
+if __name__ == "__main__":
+    cnn_train_configs = TrainerConfig(ckpt_path="./test.pt",max_epochs=40,learning_rate=4.67e-4,weight_decay=6.423e-4,num_workers=2)
+    CNN_model = MnistCNN()
+    trainer = Trainer(model=CNN_model,train_dataset=train_set,test_dataset=test_set,config=cnn_train_configs)
+    model_metrics = trainer.train()
 
-plotter = Plot(model_metrics=model_metrics)
-plotter.plot()
+    plotter = Plot(model_metrics=model_metrics)
+    plotter.plot()
